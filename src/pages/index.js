b26859cefa53f48a9f9f56561619998913ca0f4c
Tobgaybolo <141105504+Tobgaybolo@users.noreply.github.com>
2025-07-15 00:08:04 +0600

    feat:added in the resume

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,7 +39,7 @@ export default function Home() {
                 Explore my latest projects, showcasing my expertise in React.js and web development.
                 </p>
                 <div className='flex items-center self-start mt-2 lg:self-center'>
-                  <Link href="/dummy.pdf" target={"_blank"}
+                  <Link href="/Resume.docx.pdf" target={"_blank"}
                   className='flex items-center bg-dark text-light p-2.5 px-6
                   rounded-lg text-lg font-semibold hover:bg-light hover:text-dark
                   border-2 border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark
